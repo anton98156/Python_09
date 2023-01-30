22be06baeff7a1e4b560843d02db494ab73763ec
Anton <anton98156789@gmail.com>
2023-01-30 12:13:42 +0300

    Added usual commands, but log does not work

--- bot.py
+++ bot.py
@@ -1,11 +1,19 @@
+# Прикрутить бота к задачам с предыдущего семинара:
+# Создать калькулятор для работы с рациональными и комплексными числами, организовать меню, добавив в неё систему логирования
+# Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
+import os
+os.system("clear")
 
 app = ApplicationBuilder().token("6118659428:AAErWajgtqVAhADvPJKzZ9Hfx2Cc5JbOTYM").build()
 
-app.add_handler(CommandHandler("hello", hi_command))
-# app.add_handler(CommandHandler("game", game_bot))
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
 
 print('server start')
 
